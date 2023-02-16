f722781cf83f013949ceaf0b7807bf19b135637c
Abdulrahman Yasser <abdu.ya74@gmail.com>
2023-02-16 11:29:14 +0200

    Working in chapter 2, i need to edit my PWM Channel typedef

--- TivaC_MCAL/TIVA_C_MCAL/Debug/HAL/Sensor/Dynamic/src/Sensor_Lcfg.d
+++ TivaC_MCAL/TIVA_C_MCAL/Debug/HAL/Sensor/Dynamic/src/Sensor_Lcfg.d
@@ -1,6 +1,27 @@
 # FIXED
 
 HAL/Sensor/Dynamic/src/Sensor_Lcfg.obj: ../HAL/Sensor/Dynamic/src/Sensor_Lcfg.c
+HAL/Sensor/Dynamic/src/Sensor_Lcfg.obj: ../HAL/Sensor/Dynamic/inc/Sensor_Cfg.h
+HAL/Sensor/Dynamic/src/Sensor_Lcfg.obj: ../HAL/Sensor/Static/inc/Sensor_Types.h
+HAL/Sensor/Dynamic/src/Sensor_Lcfg.obj: ../Common/Platform_Types.h
+HAL/Sensor/Dynamic/src/Sensor_Lcfg.obj: ../Mcal/DIO/Dynamic/inc/DIO_Cfg.h
+HAL/Sensor/Dynamic/src/Sensor_Lcfg.obj: ../Mcal/DIO/Static/inc/DIO_Types.h
+HAL/Sensor/Dynamic/src/Sensor_Lcfg.obj: ../Common/Std_Types.h
+HAL/Sensor/Dynamic/src/Sensor_Lcfg.obj: ../Common/Compiler.h
 
 ../HAL/Sensor/Dynamic/src/Sensor_Lcfg.c:
+
+../HAL/Sensor/Dynamic/inc/Sensor_Cfg.h:
+
+../HAL/Sensor/Static/inc/Sensor_Types.h:
+
+../Common/Platform_Types.h:
+
+../Mcal/DIO/Dynamic/inc/DIO_Cfg.h:
+
+../Mcal/DIO/Static/inc/DIO_Types.h:
+
+../Common/Std_Types.h:
+
+../Common/Compiler.h:
 
--- TivaC_MCAL/TIVA_C_MCAL/Debug/HAL/Sensor/Static/src/Sensor.d
+++ TivaC_MCAL/TIVA_C_MCAL/Debug/HAL/Sensor/Static/src/Sensor.d
@@ -7,6 +7,13 @@ HAL/Sensor/Static/src/Sensor.obj: ../Common/Std_Types.h
 HAL/Sensor/Static/src/Sensor.obj: ../Common/Platform_Types.h
 HAL/Sensor/Static/src/Sensor.obj: ../Common/Compiler.h
 HAL/Sensor/Static/src/Sensor.obj: ../HAL/Sensor/Static/inc/Sensor_Types.h
+HAL/Sensor/Static/src/Sensor.obj: ../Mcal/DIO/Dynamic/inc/DIO_Cfg.h
+HAL/Sensor/Static/src/Sensor.obj: ../HAL/Sensor/Dynamic/inc/Sensor_Cfg.h
+HAL/Sensor/Static/src/Sensor.obj: ../Mcal/DIO/Static/inc/DIO.h
+HAL/Sensor/Static/src/Sensor.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h
+HAL/Sensor/Static/src/Sensor.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+HAL/Sensor/Static/src/Sensor.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+HAL/Sensor/Static/src/Sensor.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
 
 ../HAL/Sensor/Static/src/Sensor.c:
 
@@ -21,4 +28,18 @@ HAL/Sensor/Static/src/Sensor.obj: ../HAL/Sensor/Static/inc/Sensor_Types.h
 ../Common/Compiler.h:
 
 ../HAL/Sensor/Static/inc/Sensor_Types.h:
+
+../Mcal/DIO/Dynamic/inc/DIO_Cfg.h:
+
+../HAL/Sensor/Dynamic/inc/Sensor_Cfg.h:
+
+../Mcal/DIO/Static/inc/DIO.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
 
